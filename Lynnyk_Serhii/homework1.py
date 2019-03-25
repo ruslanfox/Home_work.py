398712bc0bd08fd5c1d0d8df055be17a7e76848a
mpanarin <m.panarin@mobilunity.com>
2019-03-25 09:25:59 +0200

    [FIX] existing lint errors

--- Lynnyk_Serhii/homework1.py
+++ Lynnyk_Serhii/homework1.py
@@ -1,7 +1,5 @@
 import random
 a = random.randrange(1000, 1000000)
 b = list(str(a))
-print ("Сгенерированое число:", a)
-print ("Четвертый знак:", b[-4])
-
-       
+print("Сгенерированое число:", a)
+print("Четвертый знак:", b[-4])
